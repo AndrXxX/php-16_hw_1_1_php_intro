056f937e0b834e0120f29bb1743a8792c2aaad3e
Unknown <andrew1gar@yandex.ru>
2017-09-17 21:35:39 +0300

    Auto stash before rebase of "origin/master"

---
+++ me/about.php
@@ -0,0 +1,53 @@
+<?php
+$name = 'Андрей';
+$age = 28;
+$email = 'andrew1gar@yandex.ru';
+$city = 'Ростов-на-Дону';
+$aboutMe = 'Начинающий Веб-разработчик';
+?>
+
+<!DOCTYPE>
+<html lang="ru">
+  <head>
+    <title><?= $name ?> – <?= $aboutMe ?></title>
+    <meta charset="utf-8">
+    <style>
+      body {
+        font-family: sans-serif;
+      }
+
+      dl {
+        display: table-row;
+      }
+
+      dt, dd {
+        display: table-cell;
+        padding: 5px 10px;
+      }
+    </style>
+  </head>
+
+  <body>
+    <h1>Страница пользователя <?= $name ?></h1>
+    <dl>
+      <dt>Имя:</dt>
+      <dd><?= $name ?></dd>
+    </dl>
+    <dl>
+      <dt>Возраст:</dt>
+      <dd><?= $age ?></dd>
+    </dl>
+    <dl>
+      <dt>Адрес электронной почты:</dt>
+      <dd><a href="mailto:<?= $email ?>"><?= $email ?></a></dd>
+    </dl>
+    <dl>
+      <dt>Город:</dt>
+      <dd><?= $city ?></dd>
+    </dl>
+    <dl>
+      <dt>О себе:</dt>
+      <dd><?= $aboutMe ?></dd>
+    </dl>
+  </body>
+</html>
